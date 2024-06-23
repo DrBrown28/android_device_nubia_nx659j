a69fdeb33d86f5174b1cfe2397d74c1bb98726ac
Bruno Martins <bgcngm@gmail.com>
2024-06-23 21:58:49 +0100

    nx659j: Convert WiFi firmware symlinks to install_symlink targets
    The last bit that was preventing building hermetic images.
    
    Change-Id: I75bb7de4593a7992f6015317cf97e088beeba0cb

--- Android.mk
+++ Android.mk
@@ -22,15 +22,6 @@ include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
 
-WIFI_FIRMWARE_SYMLINKS := $(TARGET_OUT_VENDOR)/firmware/wlan/qca_cld
-$(WIFI_FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
-	@echo "Creating wifi firmware symlinks: $@"
-	mkdir -p $@
-	$(hide) ln -sf /vendor/etc/wifi/WCNSS_qcom_cfg.ini $@/WCNSS_qcom_cfg.ini
-	$(hide) ln -sf /mnt/vendor/persist/wlan_mac.bin $@/wlan_mac.bin
-
-ALL_DEFAULT_INSTALLED_MODULES += $(WIFI_FIRMWARE_SYMLINKS)
-
 EGL_32_SYMLINK := $(TARGET_OUT_VENDOR)/lib/libEGL_adreno.so
 $(EGL_32_SYMLINK): $(LOCAL_INSTALLED_MODULE)
 	@echo "Creating 32-bit EGL symlink: $@"
--- device.mk
+++ device.mk
@@ -473,3 +473,8 @@ PRODUCT_PACKAGES += \
     libnl \
     libpng.vendor \
     libwfdaac_vendor
+ 
+# WiFi firmware symlinks
+ PRODUCT_PACKAGES += \
+     firmware_wlan_mac.bin_symlink \
+     firmware_WCNSS_qcom_cfg.ini_symlink    
