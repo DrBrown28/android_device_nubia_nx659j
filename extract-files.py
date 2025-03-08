2b5dcb185c0199643888203aeab0b9bf5179b72f
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    nx659j: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- extract-files.py
+++ extract-files.py
@@ -52,7 +52,6 @@ lib_fixups: lib_fixups_user_type = {
     (
         'libOmxCore',
         'libgrallocutils',
-        'libwpa_client',
     ): lib_fixup_remove,
 }
 
@@ -106,7 +105,7 @@ module = ExtractUtilsModule(
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
     namespace_imports=namespace_imports,
-    check_elf=False,
+    check_elf=True,
 )
 
 if __name__ == '__main__':
