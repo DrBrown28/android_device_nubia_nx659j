3d4dedf38e7eb49e4784d477212cbebddbe9df10
Michael Bestas <mkbestas@gmail.com>
2025-03-08 21:43:32 +0200

    nx659j: Enable ELF checks for libwpa_client dependent libs
    Change-Id: I490df02b68da4b9a7088a239672a1dcace66d270

--- device.mk
+++ device.mk
@@ -453,7 +453,6 @@ PRODUCT_PACKAGES += \
     hostapd \
     libwifi-hal-ctrl \
     libwifi-hal-qcom \
-    libwpa_client \
     vendor.qti.hardware.fstman@1.0.vendor \
     vendor.qti.hardware.wifi.hostapd@1.2.vendor \
     vendor.qti.hardware.wifi.supplicant@2.1.vendor \
