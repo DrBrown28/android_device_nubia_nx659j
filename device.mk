1a25bac357cd050c61f358ae90eae471dbbd6b96
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-05-01 17:46:42 -0400

    nx659j: add a missing camera lib android.hardware.camera.provider@2.4-impl

--- device.mk
+++ device.mk
@@ -90,11 +90,12 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.hardware.camera.provider@2.4-service_64 \
+    android.hardware.camera.provider@2.4-impl \
     android.frameworks.sensorservice@1.0.vendor \
     libcamera2ndk_vendor \
     libgui_vendor \
     libstdc++_vendor \
-    libutilscallstack.vendor \     
+    libutilscallstack.vendor \
     vendor.qti.hardware.camera.device@1.0.vendor \
     vendor.qti.hardware.camera.postproc@1.0.vendor \
     libpng.vendor \
