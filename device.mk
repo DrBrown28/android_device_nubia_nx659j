ff44d346ddc2fa31a966bf9f805b2459ecc2a4d0
DrBrown28 <112201610+DrBrown28@users.noreply.github.com>
2025-05-18 23:51:43 -0400

    nx659j: Fixup fstab.qcom path in device.mk

--- device.mk
+++ device.mk
@@ -170,7 +170,7 @@ PRODUCT_PACKAGES += \
 
 # Fstab
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/rootdir/etc/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
+    $(LOCAL_PATH)/rootdir/fstab.qcom:$(TARGET_COPY_OUT_RAMDISK)/fstab.qcom
 
 # Health
 PRODUCT_PACKAGES += \
